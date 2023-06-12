026b4a2b2ca989bd18cb0729d2fff0db7877f7cb
Rurewa <rurewa@mail.ru>
2023-06-12 17:50:43 +0300

    Update Lessons/Enum/JustEnum/main.cpp

--- Lessons/Enum/JustEnum/main.cpp
+++ Lessons/Enum/JustEnum/main.cpp
@@ -1,79 +1,52 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // Варианты доступа к элементам перечислений enum по выбору пользователя
-//
+// seasonsEnum.cpp
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <iostream>
 using namespace std;
 
-enum Seasons { SPRING, AUTUMN, SUMMER, WINTER };
+enum Seasons { AUTUMN, WINTER, SPRING, SUMMER };
 
 int main() {
-    bool exit = true; // Выбор пользователя - продолжить или выйти
-    /* while (cin >> ws >> exit) {
-        cout << "Нужно выбрать время года: весна - 0, осень - 1, лето - 2, зима - 3\n";
-        int yourSeasons = 0;
-        cin >> ws >> yourSeasons;
-        Seasons season = static_cast<Seasons>(yourSeasons);
-        switch (season)
-        {
-            case SPRING:
-                cout << "Природа оживает!\n";
-                break;int
-            case AUTUMN:
-                cout << "Листья опадают!\n";
-                break;
-            case SUMMER:
-                cout << "Душа радуется!\n";
-                break;
-            case WINTER:
-                cout << "Можно кататься на коньках.\n";
-                break;
-            default:
-                cout << "Других времён года нет!\n";
-                break;
-            }
-        cout << "Чтобы снова смотреть времена года, нажмите 1\n";
-        cin >> ws >> exit;
-    } */
+    bool exit = true; // Выбор пользователя: продолжить или выйти
     do {
-        cout << "Нужно выбрать время года: весна - 0, осень - 1, лето - 2, зима - 3\n";
-        int yourSeasons = 0;
-        cin >> ws >> yourSeasons;
-        //Seasons season = static_cast<Seasons>(yourSeasons); // Сначала так!
-        Seasons season = Seasons(yourSeasons);
+        cout << "Нужно выбрать время года: осень - 0, зима - 1, весна - 2, лето - 3\n";
+        int yourSeason = 0;
+        cin >> ws >> yourSeason;
+        //Seasons season = static_cast<Seasons>(yourSeason);
+        Seasons season = Seasons(yourSeason);
         switch (season)
         {
-            case SPRING:
-                cout << "Природа оживает!\n";
-                break;
             case AUTUMN:
-                cout << "Листья опадают!\n";
-                break;
-            case SUMMER:
-                cout << "Душа радуется!\n";
+                cout << "Листья падают\n";
                 break;
             case WINTER:
-                cout << "Можно кататься на коньках.\n";
+                cout << "Можно кататься на коньках\n";
+                break;
+            case SPRING:
+                cout << "Природа оживает\n";
+                break;
+            case SUMMER:
+                cout << "Душа радуется\n";
                 break;
             default:
                 cout << "Других времён года нет!\n";
                 break;
-            }
-        cout << "Чтобы снова смотреть времена года, нажмите 1\n";
+        }
+        cout << "Чтобы снова смотреть времена года нажмите 1\n";
         cin >> ws >> exit;
-    } while (exit);
+    } while(exit);
     return 0;
 }
 /* Output:
-Нужно выбрать время года: весна - 0, осень - 1, лето - 2, зима - 3
-3
-Природа оживает!
-Чтобы снова смотреть времена года, нажмите 1
-0
+
+
 */
 // Задание
-/* Сделать тоже самое, но с просто while (без do) */
+/*
+То же самое, но с while
+ */
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // END FILE
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
