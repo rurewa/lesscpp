0d74755da5f699850f6bfeb673362507cbd9661e
Alex <rurewa@mail.ru>
2023-08-18 08:13:17 +0300

    Update Functions/Recursion/factorialSimply.cpp

--- Lessons/Functions/main.cpp
+++ Lessons/Functions/main.cpp
@@ -1,64 +1,18 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-// Интерактивный пример связки перечисления с функциями.
-// eFunc.cpp Functions
+//
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 #include <iostream>
 using namespace std;
 
-enum class Colors {
-    RED, BROWN, GRAY, WHITE, PINK, BLUE, PURPLE
-};
-
-// Прототипы функций
-void printColor(Colors color);
-int userInput();
 
 int main() {
-    cout << "Введите номер цвета\n";
-    int userInputNum = userInput();
-    Colors color (static_cast<Colors>(userInputNum));
-    printColor(color);
+    
     return 0;
 }
 
-void printColor(Colors color) {
-    if (color == Colors::RED) {
-        puts("Red");
-    }
-    else if (color == Colors::BROWN) {
-        puts("Brown");
-    }
-    else if (color == Colors::GRAY) {
-        puts("Gray");
-    }
-    else if (color == Colors::WHITE) {
-        puts("White");
-    }
-    else if (color == Colors::PINK) {
-        puts("Pink");
-    }
-    else if (color == Colors::BLUE) {
-        puts("Blue");
-    }
-    else if (color == Colors::PURPLE) {
-        puts("Purple");
-    }
-    else {
-        puts("Что?");
-    }
-}
-
-int userInput() {
-    int userNum;
-    cin >> userNum;
-    return userNum;
-}
-
 /* Output:
-Введите номер цвета
-10
-Что?
+
 */
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
 // END FILE
--- Lessons/LogicBool/main.cpp
+++ Lessons/LogicBool/main.cpp
@@ -1,83 +1,19 @@
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
-// Формальная логика и закон Де Моргана
-// formalogic.cpp Bools
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Программа определения возможности езды на автомобиле
+// carIsWorking.cpp
 // V 1.0
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include<iostream>
 using namespace std;
 
 int main() {
-    cout << boolalpha;
-    bool a = true, b = true, c = true;
-    cout << "Ассоциативность\n";
-    cout << "(a || b) || c - " << ((a || b) || c) <<
-         " - (a || (b || c)) - " << (a || (b || c)) << endl;
-    cout << "(a && b) && c - " << ((a && b) && c) <<
-         " - a && (b && c) - " << (a && (b && c)) << endl;
-    cout << "Коммуникативность\n";
-    cout << "a && b - " << (a && b) << " b && a " << (b && a) << endl;
-    cout << "a || b - " << (a || b) << " b || a " << (b && a) << endl;
-    cout << "a == b - " << (a == b) << " b == a " << (b == a) << endl;
-    cout << "Дистрибутивность\n";
-    cout << "a && (b || c) - " << (a && (b || c))
-         << " ((a && b) || (a && c)) " << ((a && b) || (a && c)) << endl;
-    cout << "a || (b && c) - " << (a || (b && c))
-         << " (a || b) && (a || c) " << ((a || b) && (a || c)) << endl;
-    cout << "Двойное отрицание\n";
-    cout << "!!a - " << !!a << endl;
-    cout << "Закон Де Морган\n";
-    cout << "!(a && b) - " << (!(a && b)) << " !a || !b " << (!a || !b) << endl;
-    cout << "!(a || b) - " << (!(a || b)) << " !a && !b " << (!a && !b) << endl;
+
     return 0;
 }
-// Output:
+// Output
 /*
-Ассоциативность
-(a || b) || c - true - (a || (b || c)) - true
-(a && b) && c - true - a && (b && c) - true
-Коммуникативность
-a && b - true b && a true
-a || b - true b || a true
-a == b - true b == a true
-Дистрибутивность
-a && (b || c) - true ((a && b) || (a && c)) true
-a || (b && c) - true (a || b) && (a || c) true
-Двойное отрицание
-!!a - true
-[user@grip3script LogicBool]$ clang++ -Wall main.cpp -o main.bin
-[user@grip3script LogicBool]$ ./main.bin
-Ассоциативность
-(a || b) || c - true - (a || (b || c)) - true
-(a && b) && c - true - a && (b && c) - true
-Коммуникативность
-a && b - true b && a true
-a || b - true b || a true
-a == b - true b == a true
-Дистрибутивность
-a && (b || c) - true ((a && b) || (a && c)) true
-a || (b && c) - true (a || b) && (a || c) true
-Двойное отрицание
-!!a - true
-Закон Де Морган
-!(a && b) - false !a || !b false
-[user@grip3script LogicBool]$ clang++ -Wall main.cpp -o main.bin
-[user@grip3script LogicBool]$ ./main.bin
-Ассоциативность
-(a || b) || c - true - (a || (b || c)) - true
-(a && b) && c - true - a && (b && c) - true
-Коммуникативность
-a && b - true b && a true
-a || b - true b || a true
-a == b - true b == a true
-Дистрибутивность
-a && (b || c) - true ((a && b) || (a && c)) true
-a || (b && c) - true (a || b) && (a || c) true
-Двойное отрицание
-!!a - true
-Закон Де Морган
-!(a && b) - false !a || !b false
-!(a || b) - false !a && !b false
+
 */
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // END FILE
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
