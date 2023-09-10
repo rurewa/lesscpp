d6927303606b4ac17bcffb79ab711e6970cad1a9
Alex <rurewa@mail.ru>
2023-09-10 09:54:44 +0300

    Rename DecimalToBinary to DecimalAndBinary. Added DecimalAndBinary/binToDec.cpp, DecimalAndBinary/decToBin.cpp, FlowControlStatements/If_Else/IncompleteIf/A_PUSH.dia

--- Lessons/FlowControlStatements/If_Else/IncompleteIf/main.cpp
+++ Lessons/FlowControlStatements/If_Else/IncompleteIf/main.cpp
@@ -1,45 +1,17 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Проверка логина и пароля
-// checkPassLogIncompleteIf.cpp IFELSE
+// 
 // V 1.0
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #include<iostream>
 using namespace std;
 
 int main() {
-    const string LOGIN = "rus";
-    const short PASSWORD = 12345;
-    cout << "Введите логин: ";
-    string userLogin;
-    getline(cin >> ws, userLogin);
-    cout << "Введите пароль: ";
-    int userPassword = 0;
-    cin >> ws >> userPassword;
-    /* if (LOGIN != userLogin || PASSWORD != userPassword) {
-        puts("Ошибка! Попробуйте снова.");
-        main();
-    } */
-    if (!(userLogin == LOGIN && userPassword == PASSWORD)) {
-        puts("Ошибка! Попробуйте снова.");
-        main();
-    }
-    // LOGIN == userLogin && PASSWORD == userPassword
-    if (!(LOGIN != userLogin && PASSWORD != userPassword)) {
-        puts("Верно! Пропускаю");
-    }
+    
     return 0;
 }
 // Output
 /*
-Введите логин: rus
-Введите пароль: 123
-Ошибка! Попробуйте снова.
-Введите логин: rus
-Введите пароль: 1234
-Ошибка! Попробуйте снова.
-Введите логин: rus
-Введите пароль: 12345
-Верно! Пропускаю
+
 */
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // END FILE
